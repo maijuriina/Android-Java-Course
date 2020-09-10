5523196e58b1e7342ee1ef7746eb2110bb2763b9
Maijuriina Mustonen <maiju.mustonen@gmail.com>
2020-09-10 17:54:34 +0300

    Created log messages on different levels with a static String variable TAG to differentiate log messages.

--- TestApplication/app/src/main/java/com/example/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/com/example/testapplication/MainActivity.java
@@ -10,12 +10,19 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button button; // introduce button
+    public static final String TAG = "TestApplicationMessage";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button); // find button
         button.setOnClickListener(this); // this meaning this class
+
+        Log.d(TAG, "App activated successfully");
+        Log.i(TAG, "Log sent from onCreate()");
+        Log.w(TAG, "Running out of things to log!");
+        Log.e(TAG, "Fatal error! Could not find more things to log.");
 
         /*Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {

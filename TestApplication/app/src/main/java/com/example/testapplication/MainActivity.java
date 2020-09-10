968e3df4b25d998ca43c2de1b93f4f1a3ef2760c
Maijuriina Mustonen <maiju.mustonen@gmail.com>
2020-09-10 08:26:55 +0100

    Updates made for versions as suggested by AS. OnClickListener and onClick-interface created for a button which logs a message when clicked. All text in the view now comes from strings.xml instead of being hardcoded.

--- TestApplication/app/src/main/java/com/example/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/com/example/testapplication/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.testapplication;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button button; // introduce button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button); // find button
+        button.setOnClickListener(this); // this meaning this class
+
+        /*Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.e("test", "Button has been clicked!");
+            }
+        });*/
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.button:
+                Log.e("test", "Button has been clicked!");
+                break;
+        }
     }
 }
